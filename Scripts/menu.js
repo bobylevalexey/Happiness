dac0b0d76d7cc34e1ad3d30c9c53bf8f1138b860
AnnaSvalova <saa1330@gmail.com>
2014-04-15 19:56:55 +0600

    Наброски

---
+++ Scripts/menu.js
@@ -0,0 +1,17 @@
+$(function() {
+   // $('.menu a').css({'marginTop':'5px'});
+
+
+    // $('.menu > li').hover(
+    // function () {
+        // $('a',$(this)).stop().animate({
+            // 'marginTop':'0px'
+        // },200);
+    // },
+    // function () {
+        // $('a',$(this)).stop().animate({
+            // 'marginTop':'5px'
+        // },200);
+    }
+);
+});
