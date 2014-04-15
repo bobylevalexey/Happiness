09fda0da2aa9e3e95ac4a812b6a5e9e28f0aec95
AnnaSvalova <saa1330@gmail.com>
2014-04-15 22:20:35 +0600

    Дбавлено выдвижное меню

--- Scripts/menu.js
+++ Scripts/menu.js
@@ -1,16 +1,44 @@
 $(function() {
-   $('.menu a').css({'marginBottom':'-5px'});
 
+    $('.menu > a').hover(
+        function () {
+            switch ($(this).attr('class')) {
+                case 'service' :
+                case 'contact' :
+                    topA = '20px';
+                    break;
+                case 'about_us' :
+                case 'home' :
+                    topA = '-5px';
+                    break;
+                case 'feed' :
+                case 'review' :
+                    topA = '45px';
+                    break;
+            }
+            $(this).stop().animate({
+                'marginTop':topA
+            },200);
+        },
+        function () {
+            switch ($(this).attr('class')) {
+                case 'service' :
+                case 'contact' :
+                    topA = '25px';
+                    break;
+                case 'about_us' :
+                case 'home' :
+                    topA = '0px';
+                    break;
+                case 'feed' :
+                case 'review' :
+                    topA = '50px';
+                    break;
+            }
+            $(this).stop().animate({
+                'marginTop':topA
+            },200);
+        }
+    )
 
-    $('.menu').hover(
-    function () {
-        $('a',$(this)).stop().animate({
-            'marginBottom':'0px'
-        },200);
-    },
-    function () {
-        $('a',$(this)).stop().animate({
-            'marginBottom':'5px'
-        },200);
-    })
 });
